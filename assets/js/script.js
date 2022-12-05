82037b05f587f8616faaf5c0a4ead27b223c5134
Amrutha2103 <amrutha.ananthan@outlook.com>
2022-12-05 15:32:59 +0000

    Add code to question area and answer area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,34 @@
-const quiz = [
+const quizData = [
     {
-        question : "Q1:A Full Stack developer should be able to write front-end code in which of the following?",
+        question : "A Full Stack developer should be able to write front-end code in which of the following?",
         a: "Java",
         b: "Javascript",
         c: "HTML",
         d: "All of the above",
         ans:"choice4"
     },{
-        question : "Q2:Which of the following are front end technologies?",
+        question : "Which of the following are front end technologies?",
         a: "JQuery",
         b: "SASS",
         c: "AngularJS",
         d: "All of the above",
         ans:"choice4"
     },{
-        question : "Q3:Which of the following are popular databases?",
+        question : "Which of the following are popular databases?",
         a: "MySQL",
         b: "Oracle",
         c: "MongoDB",
         d: "All of the above",
         ans:"choice4"
     },{
-        question : "Q4:Why do we use JQuery?",
+        question : "Why do we use JQuery?",
         a: "Creation of mobile Application",
         b: "Creation of Interactive website",
         c: "Database connectivity",
         d: "None of the above",
         ans:"choice2"
     },{
-        question : "Q5:A Full Stack developer works on which of the following ?",
+        question : "A Full Stack developer works on which of the following ?",
         a: "Mobile Application Development",
         b: "Web Development",
         c: "Database Technologies",
@@ -36,3 +36,26 @@ const quiz = [
         ans:"choice4"
     }
 ];
+
+const question = document.querySelector('.question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+const next = document.querySelector('#next');
+
+let questionCount = 0;
+
+function loadQuestion() {
+     
+    const questionList = quizData[questionCount];
+
+    question.innerText = questionList.question;
+
+    option1.innerText = questionList.a;
+    option2.innerText = questionList.b;
+    option3.innerText = questionList.c;
+    option4.innerText = questionList.d;
+}
+
+loadQuestion();
