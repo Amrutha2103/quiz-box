aeaf76da10b8f106060956354181248c0c36da1a
Amrutha2103 <amrutha.ananthan@outlook.com>
2023-01-23 15:32:38 +0000

    Create a home page to explain the rules of the game

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+/* jshint esversion: 11 */
+
+const quizData = [{
+    question: "A Full Stack developer should be able to write front-end code in which of the following?",
+    a: "Java",
+    b: "Javascript",
+    c: "HTML",
+    d: "All of the above",
+    ans: "choice4"
+}, {
+    question: "Which of the following are front end technologies?",
+    a: "JQuery",
+    b: "SASS",
+    c: "AngularJS",
+    d: "All of the above",
+    ans: "choice4"
+}, {
+    question: "Which of the following are popular databases?",
+    a: "MySQL",
+    b: "Oracle",
+    c: "MongoDB",
+    d: "All of the above",
+    ans: "choice4"
+}, {
+    question: "Why do we use JQuery?",
+    a: "Creation of mobile Application",
+    b: "Creation of Interactive website",
+    c: "Database connectivity",
+    d: "None of the above",
+    ans: "choice2"
+}, {
+    question: "A Full Stack developer works on which of the following ?",
+    a: "Mobile Application Development",
+    b: "Web Development",
+    c: "Database Technologies",
+    d: "All of the above",
+    ans: "choice4"
+}];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,4 @@
-const quizData = [{
-    question: "A Full Stack developer should be able to write front-end code in which of the following?",
-    a: "Java",
-    b: "Javascript",
-    c: "HTML",
-    d: "All of the above",
-    ans: "choice4"
-}, {
-    question: "Which of the following are front end technologies?",
-    a: "JQuery",
-    b: "SASS",
-    c: "AngularJS",
-    d: "All of the above",
-    ans: "choice4"
-}, {
-    question: "Which of the following are popular databases?",
-    a: "MySQL",
-    b: "Oracle",
-    c: "MongoDB",
-    d: "All of the above",
-    ans: "choice4"
-}, {
-    question: "Why do we use JQuery?",
-    a: "Creation of mobile Application",
-    b: "Creation of Interactive website",
-    c: "Database connectivity",
-    d: "None of the above",
-    ans: "choice2"
-}, {
-    question: "A Full Stack developer works on which of the following ?",
-    a: "Mobile Application Development",
-    b: "Web Development",
-    c: "Database Technologies",
-    d: "All of the above",
-    ans: "choice4"
-}];
+/* jshint esversion: 11 */
 
 /**
  * Declare constants for DOM elements 
@@ -59,7 +24,8 @@ let scores = 0;
 
 function loadQuestion() {
 
-    const questionList = quizData[questionCount];
+    let randomize = quizData.sort(() => Math.random() - .5);
+    const questionList = randomize[questionCount];
     question.innerText = questionList.question;
 
     option1.innerText = questionList.a;
