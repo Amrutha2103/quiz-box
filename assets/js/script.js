8bd46d1c5c911eabd470a56060833bcf5af77748
Amrutha2103 <amrutha.ananthan@outlook.com>
2023-01-26 16:13:37 +0000

    Add timer function to the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,3 +93,21 @@ next.addEventListener('click', () => {
         score.classList.remove('score-area');
     }
 });
+
+// Add timer to the questions
+
+const startingMinutes = 5;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown(){
+    const minutes = Math.floor(time/ 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds: seconds;
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
