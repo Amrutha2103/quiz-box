5f2b6bcba3c21873e509e6f87bcde8677b21fa0c
Amrutha2103 <amrutha.ananthan@outlook.com>
2023-01-27 10:53:13 +0000

    Update the timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,7 @@ function getCheckAnswer() {
     let answer;
 
     answers.forEach((curAnsElem) => {
-        
+
         if (curAnsElem.checked) {
             answer = curAnsElem.id;
         }
@@ -50,7 +50,7 @@ function getCheckAnswer() {
 }
 
 answers.forEach((curAnsElem) => {
-    curAnsElem.addEventListener("click", function() {
+    curAnsElem.addEventListener("click", function () {
         next.classList.remove("hide");
     });
 });
@@ -84,30 +84,40 @@ next.addEventListener('click', () => {
     if (questionCount < quizData.length) {
         loadQuestion();
     } else {
-        question.classList.add("hide");
+        endGame();
+    }
+});
+
+function endGame() {
+    question.classList.add("hide");
         choicesUl.classList.add("hide");
         score.innerHTML = `
             <h3> You scored ${scores}/${quizData.length} </h3>
             <button class="btn" onclick="location.reload()"> Reload </button>
             `;
         score.classList.remove('score-area');
-    }
-});
+}
 
 // Add timer to the questions
 
-const startingMinutes = 5;
+const startingMinutes = 1;
 let time = startingMinutes * 60;
 
 const countdownEl = document.getElementById('countdown');
 
-setInterval(updateCountdown, 1000);
+let timerinterval = setInterval(updateCountdown, 1000);
 
-function updateCountdown(){
-    const minutes = Math.floor(time/ 60);
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
     let seconds = time % 60;
 
-    seconds = seconds < 10 ? '0' + seconds: seconds;
-    countdownEl.innerHTML = `${minutes}:${seconds}`;
-    time--;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    if (minutes <= 0 && seconds <= 0) {
+        clearInterval(timerinterval);
+        countdownEl.innerHTML = `time up`;
+        endGame();
+    } else {
+        countdownEl.innerHTML = `${minutes}:${seconds}`;
+        time--;
+    }
 }
